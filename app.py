78c1230d1644beefcdda1956a8ca810020e2c486
Lagishetti-Venkatesh <venkatesh29.12.1997@gmail.com>
2022-09-06 12:19:07 +0530

    final_touch

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "CI CD pipeline got created"
+    return "CI CD pipeline got created. No need of creating Docker images to upload the changes to the deployment"
 
 if __name__ == "__main__":
     app.run(debug=True)
